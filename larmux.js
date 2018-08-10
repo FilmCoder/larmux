b8134588179726d3350c3a37b1ac1053e9f8ae4e
Filmcoder <dominicnapoleon@Dominics-MBP.hsd1.va.comcast.net>
2018-08-10 13:22:07 -0400

    larmuxify repeats until it changes something

--- larmux.js
+++ larmux.js
@@ -13,53 +13,72 @@ function setOutputSwag(text) {
 }
 
 function larmuxify(input_text) {
-    var words = input_text.split(" ");
-    var output_text = [];
-    const larmallah = 'larmallah ';
-    var dictionary = {'please':'plix', 'moon':'luneplop', 'dog':'dorgle', 'cat':'catsyprip'};
-    for (word of words) {
-        const num = Math.random();
-        // console.log(num);
-        var lastChar = word.substr(word.length - 1);
-        var word_length = word.length - 1;
 
-        if (word in dictionary) {
-            word = dictionary[word];
-        }
-        else if (lastChar == '.' && num > 0.75) {
-            if (num > 0.95) {
-                word = word.concat(' Swag swag!');
-            }
-            else if (num > 0.9) {
-                word = word.concat(' Plix.');
-            }
-            else {
-                word = word.concat(' Swag.');
-            }
-        }
-        else if (num > 0.97) {
-            word = larmallah.concat(word).concat('bar');
-        }
-        else if (num > 0.92) {
-            word = word.concat('bles');
-        }
-        else if (num > 0.87) {
-            word = word.concat('zorz');
-        }
-        else if (num > 0.82) {
-            word = word.concat('sles');
-        }
-        else if (num > 0.77) {
-            word = word.concat('sies');
-        }
+    // TODO: switch to do while loop, don't do while true with break and continue
+    while(true) {
+      var words = input_text.split(" ");
+      var output_text = [];
+      const larmallah = 'larmallah ';
+      var dictionary = {'please':'plix', 'moon':'luneplop', 'dog':'dorgle', 'cat':'catsyprip'};
+      for (word of words) {
+          const num = Math.random();
+          // console.log(num);
+          var lastChar = word.substr(word.length - 1);
+          var word_length = word.length - 1;
 
-        if (word.charAt(word_length) == ',') {
-            word = word.replace(',','');
-            word = word.concat(',');
-        }
-        output_text.push(word);
+          if (word in dictionary) {
+              word = dictionary[word];
+          }
+          else if (lastChar == '.' && num > 0.75) {
+              if (num > 0.95) {
+                  word = word.concat(' Swag swag!');
+              }
+              else if (num > 0.9) {
+                  word = word.concat(' Plix.');
+              }
+              else {
+                  word = word.concat(' Swag.');
+              }
+          }
+          else if (num > 0.97) {
+              word = larmallah.concat(word).concat('bar');
+          }
+          else if (num > 0.92) {
+              word = word.concat('bles');
+          }
+          else if (num > 0.87) {
+              word = word.concat('zorz');
+          }
+          else if (num > 0.82) {
+              word = word.concat('sles');
+          }
+          else if (num > 0.77) {
+              word = word.concat('sies');
+          }
 
+          if (word.charAt(word_length) == ',') {
+              word = word.replace(',','');
+              word = word.concat(',');
+          }
+          output_text.push(word);
+
+      }
+      output = output_text.join(' ');
+
+      // if the output hasn't been changed at all, run algorithm again
+      // until it's changed
+
+
+      if(output == input_text) {
+          // console.log("-----------");
+          // console.log("REDOING ALGORITHM");
+          // console.log("output: " + output);
+          // console.log("input_text: " + input_text);
+          continue;
+      } else {
+        break;
+      }
     }
-    output = output_text.join(' ');
+
     return output;
 }
